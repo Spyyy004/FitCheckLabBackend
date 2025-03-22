d4251cc4c59e65579a0111c3b119f29de7c02249
Shubham1Jain <shubham.jain@mosaicwellness.in>
2025-03-22 13:23:51 +0530

    occasions api

--- routes/occasions.js
+++ routes/occasions.js
@@ -1,28 +1,29 @@
 import express from "express";
 import { supabase } from "../config/supabaseClient.js";
-
+import authMiddleware from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 // Add an Occasion
 router.post("/add", async (req, res) => {
-  const { user_id, occasion_type, event_name, date_time, recurring, notes } = req.body;
+  const userId = req?.user?.id;
+  const { occasion, name, date_time, recurring, season, outfit_id } = req.body;
 
   const { data, error } = await supabase
     .from("occasions")
-    .insert([{ user_id, occasion_type, event_name, date_time, recurring, notes }]);
+    .insert([{ user_id: userId, occasion, name, date_time, recurring, season, outfit_id }]);
 
   if (error) return res.status(400).json({ error: error.message });
   res.json({ message: "Occasion added successfully", occasion: data });
 });
 
 // Get Occasions for a User
-router.get("/:user_id", async (req, res) => {
-  const { user_id } = req.params;
+router.get("/",authMiddleware, async (req, res) => {
+  const userId = req?.user?.id;
 
   const { data, error } = await supabase
     .from("occasions")
     .select("*")
-    .eq("user_id", user_id);
+    .eq("user_id", userId);
 
   if (error) return res.status(400).json({ error: error.message });
   res.json({ occasions: data });
